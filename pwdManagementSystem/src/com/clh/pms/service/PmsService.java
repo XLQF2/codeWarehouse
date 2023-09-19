42e2e9eab25d23df253bbb888395e8a6ba5eccd4
chenluohong <cluohong@163.com>
2023-09-19 23:15:56 +0800

    v2

--- pwdManagementSystem/src/com/clh/pms/service/PmsService.java
+++ pwdManagementSystem/src/com/clh/pms/service/PmsService.java
@@ -16,26 +16,27 @@ public class PmsService {
         return str.matches(pattern);
     }
 
-    //（1）将每个字符的ASCII码加上它在字符串中的位置(1开始)和偏移值3
+    //加密，将每个字符的ASCII码加上它在字符串中的位置(1开始)和偏移值3
     public void encryptPassword(StringBuffer pwd) {
         for (int i = 0; i < pwd.length(); i++) {
             pwd.setCharAt(i, (char)(pwd.charAt(i) + (i + 1) + 3));
         }
     }
+    //解码，将每个字符的ASCII码减去它在字符串中的位置(1开始)和偏移值3
     public void decryptPassword(StringBuffer pwd) {
         for (int i = 0; i < pwd.length(); i++) {
             pwd.setCharAt(i, (char)(pwd.charAt(i) - (i + 1) - 3));
         }
     }
 
-    //（2）将字符串的第一位和最后一位调换顺序
+    //将字符串的第一位和最后一位调换顺序
     public void exchangePassword(StringBuffer pwd) {
         pwd.setCharAt(0, (char) (pwd.charAt(0)^ pwd.charAt(pwd.length()-1)));
         pwd.setCharAt(pwd.length()-1, (char) (pwd.charAt(0)^ pwd.charAt(pwd.length()-1)));
         pwd.setCharAt(0, (char) (pwd.charAt(0)^ pwd.charAt(pwd.length()-1)));
     }
 
-    //（3）将字符串反转
+    //将字符串反转
     public void reversePasswords(StringBuffer pwd){
         for (int i = 0, j = pwd.length() - 1; i < j; i++,j--) {
             pwd.setCharAt(i, (char) (pwd.charAt(i)^ pwd.charAt(j)));
